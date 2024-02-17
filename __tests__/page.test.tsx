c66124280408acc6ae6a13507e3494f830f2a82f
unknown <rahul.kandra@slu.edu>
2024-02-17 12:52:26 -0600

    First TDD

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,8 +1,11 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import Page from '../src/app/page'
- 
-test('Page', () => {
-  render(<Page />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Hello' })).toBeDefined()
-})
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "../src/app/page";
+
+test("Page", () => {
+  render(<Page />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "Hello" })
+  ).toBeDefined();
+  expect(screen.getByText("Contact Organiser:"));
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <Link href="/dashboard">Dashboard</Link>
-    </div>
+    <>
+      <div>
+        <h1>Hello</h1>
+        <Link href="/dashboard">Dashboard</Link>
+      </div>
+      <div id="mail">
+        <h1>Contact Organiser:</h1>
+        <h2>Email: maltucker@gmail.com</h2>
+      </div>
+    </>
   );
 }
