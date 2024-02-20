09dfd0d325b4189bca9e84bbc94bab62f4c5a948
Lavanya Chand <lavanya.chand@slu.edu>
2024-02-19 18:28:39 -0600

    Demo1 Team 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,61 @@
 import Link from "next/link";
+import { sql } from "@vercel/postgres";
 
-export default function Page() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Link href="/dashboard">Dashboard</Link>
-    </div>
-  );
+export default async function Page() {
+  try {
+    const { rows } = await sql`SELECT * FROM Elevate;`;
+    const sortedRows = [...rows].sort((a, b) => b.points - a.points);
+
+    return (
+      <div>
+        <h1>Hello</h1>
+        <Link href="/dashboard">Go to Dashboard</Link>
+        <h2>2023 Final Standings</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Team</th>
+              <th></th>
+              <th>W</th>
+              <th>D</th>
+              <th>L</th>
+              <th>GD</th>
+              <th>Pts</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedRows.map((row, index) => (
+              <tr key={index}>
+                <td>{row.team}</td>
+                <td>
+                  <img
+                    src={`/logos/${row.team}.jpeg`}
+                    alt={`Logo of ${row.team}`}
+                  />
+                </td>
+                <td>{row.wins}</td>
+                <td>{row.draws}</td>
+                <td>{row.lost}</td>
+                <td>{row.gd}</td>
+                <td>{row.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data from the database:", error);
+
+    return (
+      <div>
+        <h1>Hello</h1>
+        <Link href="/dashboard">Go to Dashboard</Link>
+        <p>
+          Error fetching data from the database. Please check the console for
+          more details.
+        </p>
+      </div>
+    );
+  }
 }
