fadec919f1d48b57e9ccf98cc28a67c5f89144a7
HarithaParmar <harithaparmar23@gmail.com>
2024-02-22 13:13:11 -0600

    team3

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,9 +1,18 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Page from "../src/app/page";
+import CreateTable  from "../src/app/api/create-table-deskchampions";
 
 test("Page", () => {
   render(<Page />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "Hello" })
+  ).toBeDefined();
+  expect(screen.getByText("Contact Organiser:"));
+});
+
+test("CreateTable", () => {
+  render(<CreateTable />);
   expect(
     screen.getByRole("heading", { level: 1, name: "Hello" })
   ).toBeDefined();
