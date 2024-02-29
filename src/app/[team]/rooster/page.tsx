9c62853ca68571866f78244080cd852bf05a5ca5
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2024-02-28 20:56:47 -0600

    Players cards are visible

--- src/app/[team]/rooster/page.tsx
+++ src/app/[team]/rooster/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useEffect, useState } from "react";
+import Card from "../../Components/Card/Card";
 import Image from "next/image";
-
+import styles from "./styles.module.css";
 interface RSVPProps {
   params: {
     team: string;
@@ -29,122 +30,120 @@ interface Row {
   nov_26: string;
 }
 
+const fetchData = async (team: string, setRsvpData: any, setLoading: any) => {
+  try {
+    setLoading(true);
+    const response = await fetch(`/api/${team}/rsvp`);
+    const data = await response.json();
+    const rows = data.rows;
+    setRsvpData(rows);
+  } catch (error) {
+    console.error("Error fetching RSVP data:", error);
+  } finally {
+    setLoading(false);
+  }
+};
+
 export default function RSVP({ params }: RSVPProps) {
   const [rsvpData, setRsvpData] = useState<Row[]>([]);
   const [loading, setLoading] = useState(true);
-  // const handleChange = async (event: any, row: Row) => {
-  //   await fetch(`/api/${params.team}/rsvp`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       player_name: row.player_name,
-  //     }),
-  //   });
-  //   window.location.reload();
-  // };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(`/api/${params.team}/rsvp`);
-        const data = await response.json();
-        const rows = data.rows;
-        setRsvpData(rows);
-      } catch (error) {
-        console.error("Error fetching RSVP data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
+    fetchData(params.team, setRsvpData, setLoading);
   }, [params.team]);
 
-  if (loading) {
-    return (
-      <main>
-        <h1>... Updating Give me a Second..</h1>
-      </main>
-    );
-  }
-  const validTeamContent = (
-    <main>
-      <h1>ROOSTER FOR {params.team.toUpperCase()}</h1>
-      <Image
-        src={`/logos/${params.team}.jpeg`}
-        alt="Team logo"
-        width={100}
-        height={100}
-        style={{
-          display: "flex",
-          margin: "auto",
-          marginBottom: "-20px",
-        }}
-      />
-      <center>
-        <table
-          style={{
-            marginLeft: "-1%",
-            marginTop: "5%",
-          }}
-        >
-          <thead>
-            <tr>
-              <th
-                style={{
-                  textAlign: "center",
-                  fontWeight: "bold",
-                }}
-              >
-                Player
-              </th>
-              <th>Position</th>
-              <th>Commitment Level</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rsvpData.map((row) => (
-              <tr key={row.player_name}>
-                <td style={{ textAlign: "center", fontWeight: "bold" }}>
-                  {row.player_name}
-                </td>
-                <td>
-                  <select>
-                    <option value="Goalkeeper">Goalkeeper</option>
-                    <option value="Defender">Defender</option>
-                    <option value="Midfielder">Midfielder</option>
-                    <option value="Forward">Forward</option>
-                  </select>
-                </td>
-                <td>
-                  <select>
-                    <option value="fulltime">Full Time</option>
-                    <option value="parttime">Part Time</option>
-                  </select>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <button>Add Player</button>
-        <button>Remove Player</button>
-      </center>
-    </main>
+  return loading ? (
+    <LoadingMessage />
+  ) : validTeams.includes(params.team) ? (
+    <ValidTeamContent params={params} rsvpData={rsvpData} />
+  ) : (
+    <InvalidTeamContent params={params} />
   );
-
-  const invalidTeamContent = (
-    <main>
-      <h1>Invalid Team</h1>
-      <p>Sorry 🙃, No RSVP for {params.team.toUpperCase()} </p>
-      We will be adding more teams soon.
-      <h1>😊</h1>
-    </main>
-  );
-
-  return validTeams.includes(params.team)
-    ? validTeamContent
-    : invalidTeamContent;
 }
+
+const LoadingMessage = () => (
+  <main>
+    <h1>... Updating Give me a Second..</h1>
+  </main>
+);
+
+const ValidTeamContent = ({
+  params,
+  rsvpData,
+}: {
+  params: { team: string };
+  rsvpData: Row[];
+}) => (
+  <main>
+    <h1>ROOSTER FOR {params.team.toUpperCase()}</h1>
+    <TeamLogo team={params.team} />
+    <br />
+    <PlayerTable params={params} rsvpData={rsvpData} />
+  </main>
+);
+
+const InvalidTeamContent = ({ params }: { params: { team: string } }) => (
+  <main>
+    <h1>Invalid Team</h1>
+    <p>Sorry 🙃, No RSVP for {params.team.toUpperCase()} </p>
+    We will be adding more teams soon.
+    <h1>😊</h1>
+  </main>
+);
+
+const TeamLogo = ({ team }: { team: string }) => (
+  <Image
+    src={`/logos/${team}.jpeg`}
+    alt="Team logo"
+    width={100}
+    height={100}
+    style={{
+      display: "flex",
+      margin: "auto",
+      marginBottom: "-20px",
+    }}
+  />
+);
+const PlayerTable = ({
+  params,
+  rsvpData,
+}: {
+  params: { team: string };
+  rsvpData: Row[];
+}) => (
+  <center>
+    <div className={styles.cardContainer}>
+      {rsvpData.map((row) => (
+        <Card
+          key={row.player_name}
+          player_name={row.player_name}
+          player_commitment="Full Time"
+          player_position="Mid Fielder"
+          player_team={params.team}
+        />
+      ))}
+    </div>
+    <button
+      style={{
+        margin: "10px",
+        backgroundColor: "green",
+        color: "white",
+        width: "150px",
+        height: "50px",
+      }}
+    >
+      Add Player
+    </button>
+    <button
+      style={{
+        margin: "10px",
+        backgroundColor: "red",
+        color: "white",
+        width: "150px",
+        height: "50px",
+      }}
+    >
+      Remove Player
+    </button>
+  </center>
+);
