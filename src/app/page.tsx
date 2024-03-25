1b2806dbd6f7b96b0ba0bb42859a1f75353df9ed
Jack Belyeu <jackbelyeu@gmail.com>
2024-03-25 12:16:21 -0500

    merged year dropdown function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,127 @@
-import Link from "next/link";
-import { sql } from "@vercel/postgres";
+"use client";
+
+import React, { useEffect, useState } from "react";
+
 import Image from "next/image";
-export default async function Page() {
-  try {
-    const { rows } = await sql`
-     select * from StanDings order by points desc, goal_difference desc, wins desc, team asc;
-    `;
-    const sortedRows = [...rows].sort((a, b) => b.points - a.points);
-    return (
-      <div>
-        <br />
-        <center>
-          {" "}
-          <h1>Flagrant Fowl Futbol Association</h1>
-          <h2>2023 Final Standings</h2>
-        </center>
-        <table>
-          <thead>
-            <tr>
-              <th>Team</th>
-              <th></th>
-              <th>W</th>
-              <th>D</th>
-              <th>L</th>
-              <th>GD</th>
-              <th>Pts</th>
-              <th>MP</th>
+
+interface Row {
+  team: string;
+
+  wins: number;
+
+  draws: number;
+
+  loses: number;
+
+  goalsdifference: number;
+
+  points: number;
+
+  year: number;
+
+  matchesplayed: number;
+}
+
+export default function Page() {
+  const [pointsData, setPointsData] = useState<Row[]>([]);
+
+  const [filteredPointsData, setFilteredPointsData] = useState<Row[]>([]);
+
+  const fetchPointsData = async () => {
+    try {
+      const response = await fetch("/api/points");
+
+      const data = await response.json();
+
+      setPointsData(data.result.rows);
+    } catch (error) {
+      console.error("Error fetching points data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPointsData();
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const year = parseInt(event.target.value);
+
+    const filteredData = pointsData.filter((row) => row.year === year);
+
+    setFilteredPointsData(filteredData);
+  };
+
+  return (
+    <div>
+      <center>
+        {" "}
+        <h1>Flagrant Fowl Futbol Association</h1>
+        <h2>2023 Final Standings</h2>
+        <select onChange={handleChange}>
+          <option value="">Select Year</option>
+
+          <option value="2024">2024</option>
+
+          <option value="2023">2023</option>
+        </select>
+      </center>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Team</th>
+
+            <th></th>
+
+            <th>Wins</th>
+
+            <th>Draws</th>
+
+            <th>Loses</th>
+
+            <th>Goals Difference</th>
+
+            <th>Points</th>
+
+            <th>Matches Played</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {filteredPointsData.map((row, index) => (
+            <tr key={index}>
+              <td>{row.team}</td>
+
+              <td>
+                <Image
+                  src={`/logos/${row.team.toLowerCase()}.jpeg`}
+                  alt={`Logo of ${row.team}`}
+                  width={50}
+                  height={50}
+                />
+              </td>
+
+              <td>{row.wins}</td>
+
+              <td>{row.draws}</td>
+
+              <td>{row.loses}</td>
+
+              <td>{row.goalsdifference}</td>
+
+              <td>{row.points}</td>
+
+              <td>{row.matchesplayed}</td>
             </tr>
-          </thead>
-          <tbody>
-            {sortedRows.map((row, index) => (
-              <tr key={index}>
-                <td>{row.team.toUpperCase()}</td>
-                <td>
-                  <Image
-                    src={`/logos/${row.team.toLowerCase()}.jpeg`}
-                    alt={`Logo of ${row.team}`}
-                    width={50}
-                    height={50}
-                  />
-                </td>
-                <td>{row.wins}</td>
-                <td>{row.draws}</td>
-                <td>{row.losses}</td>
-                <td>{row.goal_difference}</td>
-                <td>{row.points}</td>
-                <td>{row.matches_played}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  } catch (error) {
-    console.error("Error fetching data from the database:", error);
-    return (
-      <div>
-        <h1>Hello</h1>
-        <Link href="/dashboard">Go to Dashboard</Link>
-        <p>
-          Error fetching data from the database. Please check the console for
-          more details.
-        </p>
-      </div>
-    );
-  }
+          ))}
+
+          {filteredPointsData.length === 0 && (
+            <tr>
+              <td colSpan={8}>Please Select a year to get the Points</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
