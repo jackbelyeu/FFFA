85c23f460fd350e30b186d05349665f1df20cbfd
prshanthreddy <varangantipr@gmail.com>
2024-03-29 17:31:14 -0500

    Removed pcfc Logo

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,8 @@
 export { default } from "next-auth/middleware";
 export const config = {
-  matcher: ["/:team/roster","/:team/rsvp","/organiserlogin"],
+  matcher: [
+    // "/:team/roster",
+    // "/:team/rsvp",
+    "/organiserlogin",
+  ],
 };
