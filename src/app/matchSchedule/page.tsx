20f3fd283a4c451193d11f3c7f02abed08a4e2ca
prshanthreddy <varangantipr@gmail.com>
2024-03-30 12:53:58 -0500

    Added Accordian for better readability

--- src/app/matchSchedule/page.tsx
+++ src/app/matchSchedule/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Match from "@/app/Components/scheduleCard/match";
-import Button from "react-bootstrap/Button";
+import Accordion from "react-bootstrap/Accordion";
+import styles from "./styles.module.css";
 
 const MatchSchedule = () => {
   const [rows, setRows] = useState([]);
@@ -47,55 +48,75 @@ const MatchSchedule = () => {
           futureMatches.length < 1 &&
           pastMatches.length < 1 && <h2>No matches scheduled</h2>}
         {todayMatches.length == 0 && <h2>No Matches Today</h2>}
-        {todayMatches.length > 0 && (
-          <>
-            <h2>Today&apos;s Matches</h2>
-            {todayMatches.map((row: any) => (
-              <Match
-                key={row.match_id}
-                match_id={row.match_id}
-                home_team={row.home_team}
-                away_team={row.away_team}
-                time={row.time}
-                date={row.date}
-                location={row.location}
-              />
-            ))}
-          </>
-        )}
-
-        {futureMatches.length > 0 && (
-          <>
-            <h2>Upcoming Matches</h2>
-            {futureMatches.map((row: any) => (
-              <Match
-                key={row.match_id}
-                match_id={row.match_id}
-                home_team={row.home_team}
-                away_team={row.away_team}
-                time={row.time}
-                date={row.date}
-                location={row.location}
-              />
-            ))}
-          </>
-        )}
-        {pastMatches.length > 0 && (
-          <>
-            <h2>Past Matches</h2>
-            {pastMatches.map((row: any) => (
-              <Match
-                key={row.match_id}
-                match_id={row.match_id}
-                home_team={row.home_team}
-                away_team={row.away_team}
-                time={row.time}
-                date={row.date}
-                location={row.location}
-              />
-            ))}
-          </>
-        )}
+          
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+              <h2>Today&apos;s Matches</h2>
+            </Accordion.Header>
+            <Accordion.Body>
+              {todayMatches.length > 0 && (
+                <>
+                  {todayMatches.map((row: any) => (
+                    <Match
+                      key={row.match_id}
+                      match_id={row.match_id}
+                      home_team={row.home_team}
+                      away_team={row.away_team}
+                      time={row.time}
+                      date={row.date}
+                      location={row.location}
+                    />
+                  ))}
+                </>
+              )}
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>
+              <h2>Upcoming Matches</h2>
+            </Accordion.Header>
+            <Accordion.Body>
+              {futureMatches.length > 0 && (
+                <>
+                  {futureMatches.map((row: any) => (
+                    <Match
+                      key={row.match_id}
+                      match_id={row.match_id}
+                      home_team={row.home_team}
+                      away_team={row.away_team}
+                      time={row.time}
+                      date={row.date}
+                      location={row.location}
+                    />
+                  ))}
+                </>
+              )}
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>
+              <h2>Past Matches</h2>
+            </Accordion.Header>
+            <Accordion.Body>
+              {pastMatches.length > 0 && (
+                <>
+                  {pastMatches.map((row: any) => (
+                    <Match
+                      key={row.match_id}
+                      match_id={row.match_id}
+                      home_team={row.home_team}
+                      away_team={row.away_team}
+                      time={row.time}
+                      date={row.date}
+                      location={row.location}
+                    />
+                  ))}
+                </>
+              )}
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </center>
       <br />
     </div>
