dc7e61904e97ab14fab0a60a5cf7e01e97846a97
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2024-04-20 13:52:31 -0500

    fix select

--- src/app/[team]/page.tsx
+++ src/app/[team]/page.tsx
@@ -50,110 +50,127 @@ export default function RosterSchedule({ params }: RosterScheduleProps) {
   return (
     <div>
       <br />
-      <h1>Schedule and Roster for {decodedTeamName}</h1>
+      <h1>Schedule and Roster </h1>
       <br />
       <center>
-      <h2>Select a Team:</h2>
-      <select
-        value={selectedTeam} 
-        onChange={(e) => {
-          const selectedTeam = Number(e.target.value);
-          setSelectedTeam(selectedTeam);
-          const teamName = decodeURIComponent(teamNames[selectedTeam]);
-          router.push(`/${teamName}`);
-        }}
-      >
-        {Object.entries(teamNames).map(([teamId, teamName]) => (
-          <option key={teamId} value={teamId}>
-            {teamName}
-          </option>
-        ))}
-      </select>
-      <br />
-      <br />
-      <Tabs
-        defaultActiveKey="home"
-        id="justify-tab-example"
-        className="mb-3"
-        justify
-      >
-        <Tab eventKey="home" title="Home">
-          <div>
-            <h2>Welcome to {decodedTeamName} </h2>
-            <p>Here you can find the schedule and roster for the team</p>
-            <Image
-              src={`/logos/${decodedTeamName}.jpeg`}
-              alt={`Logo of the ${decodedTeamName} team`}
-              width={100}
-              height={100}
-            />
-          </div>
-        </Tab>
-        <Tab eventKey="schedule" title="Schedule">
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Location</th>
-                <th>Opponent</th>
-              </tr>
-            </thead>
-            <tbody>
-              {schedule.map(
-                (match: {
-                  match_id: string;
-                  date: string;
-                  time: string;
-                  locationname: string;
-                  awayteamname: string;
-                  hometeamname: string;
-                }) => (
-                  <tr key={match.match_id}>
-                    <td>{match.date.split("T")[0]}</td>
-                    <td>{match.time}</td>
-                    <td>{match.locationname}</td>
-                    <td>
-                      {match.hometeamname === decodedTeamName
-                        ? match.awayteamname
-                        : match.hometeamname}
-                    </td>
-                  </tr>
-                )
-              )}
-            </tbody>
-          </table>
-        </Tab>
-        <Tab eventKey="longer-tab" title="Players">
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-                <th>Player Name</th>
-                <th>Position</th>
-                <th>Commitment</th>
-              </tr>
-            </thead>
-            <tbody>
-              {players.map((player: { player_name: string; position: string;commitment:string }) => (
-                <tr key={player.player_name}>
-                  <td>
-                    <Image
-                      src={`/logos/${decodedTeamName}.jpeg`}
-                      alt={`Logo of the ${decodedTeamName} team`}
-                      width={50}
-                      height={50}
-                    />
-                  </td>
-                  <td>{player.player_name}</td>
-                  <td>{player.position}</td>
-                  <td>{player.commitment}</td>
+        <h2>Select a Team:</h2>
+        <select
+          value={selectedTeam}
+          onChange={(e) => {
+            const selectedTeam = Number(e.target.value);
+            setSelectedTeam(selectedTeam);
+            const teamName = decodeURIComponent(teamNames[selectedTeam]);
+            router.push(`/${teamName}`);
+          }}
+          style={{
+            borderRadius: "5px",
+            marginBottom: "10px",
+            width:"200px",
+            height:"30px",
+            backgroundColor:"#0d6efd",
+            color:"white",
+            fontWeight:"bolder",
+            textAlign:"center"
+          }}
+        >
+          {Object.entries(teamNames).map(([teamId, teamName]) => (
+            <option key={teamId} value={teamId}>
+              {teamName}
+            </option>
+          ))}
+        </select>
+
+        <br />
+        <br />
+        <Tabs
+          defaultActiveKey="schedule"
+          id="justify-tab-example"
+          className="mb-3"
+          justify
+        >
+          <Tab eventKey="home" title="Home">
+            <div>
+              <h2>Welcome to {decodedTeamName} </h2>
+              <p>Here you can find the schedule and roster for the team</p>
+              <Image
+                src={`/logos/${decodedTeamName}.jpeg`}
+                alt={`Logo of the ${decodedTeamName} team`}
+                width={100}
+                height={100}
+              />
+            </div>
+          </Tab>
+          <Tab eventKey="schedule" title="Schedule">
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Location</th>
+                  <th>Opponent</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </Tab>
-      </Tabs>
+              </thead>
+              <tbody>
+                {schedule.map(
+                  (match: {
+                    match_id: string;
+                    date: string;
+                    time: string;
+                    locationname: string;
+                    awayteamname: string;
+                    hometeamname: string;
+                  }) => (
+                    <tr key={match.match_id}>
+                      <td>{match.date.split("T")[0]}</td>
+                      <td>{match.time}</td>
+                      <td>{match.locationname}</td>
+                      <td>
+                        {match.hometeamname === decodedTeamName
+                          ? match.awayteamname
+                          : match.hometeamname}
+                      </td>
+                    </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </Tab>
+          <Tab eventKey="longer-tab" title="Players">
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Player Name</th>
+                  <th>Position</th>
+                  <th>Commitment</th>
+                </tr>
+              </thead>
+              <tbody>
+                {players.map(
+                  (player: {
+                    player_name: string;
+                    position: string;
+                    commitment: string;
+                  }) => (
+                    <tr key={player.player_name}>
+                      <td>
+                        <Image
+                          src={`/logos/${decodedTeamName}.jpeg`}
+                          alt={`Logo of the ${decodedTeamName} team`}
+                          width={50}
+                          height={50}
+                        />
+                      </td>
+                      <td>{player.player_name}</td>
+                      <td>{player.position}</td>
+                      <td>{player.commitment}</td>
+                    </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </Tab>
+        </Tabs>
       </center>
     </div>
   );
