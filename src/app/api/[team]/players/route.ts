1053d136b2adfbb26aa0a685d49eaaf04d8328fa
Saaisathish <sa.saaisathish@gmail.com>
2024-04-21 12:01:03 -0500

    Saaisathish and Varshith Bug fix for PC db retreival

--- src/app/api/[team]/players/route.ts
+++ src/app/api/[team]/players/route.ts
@@ -5,10 +5,23 @@ export async function GET(
   request: Request,
   { params }: { team: string; params: any }
 ) {
-  const result = await sql`
-   SELECT player_name, position,commitment
-   FROM RSVP where player_team = ${params.team}
-   ORDER BY id ASC;
-  `;
-  return NextResponse.json(result);
+  // Extract the team name from the params
+  let team = params.team.toLowerCase();
+
+  // Check if team is "Pacey Chickens" and change it to "chickens"
+  if (team === "pacey chickens") {
+    team = "chickens";
+  }
+
+  try {
+    const result = await sql`
+      SELECT player_name, position, commitment
+      FROM RSVP
+      WHERE LOWER(player_team) = ${team}
+      ORDER BY id ASC;
+    `;
+    return NextResponse.json(result);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
