cc8fb28f66aa8585c6497546d69baa25bbc019a4
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2024-04-22 18:33:54 -0500

    This mostly {except for cache } fixes delete match bug

--- src/app/Components/scheduleCard/matchscore.tsx
+++ src/app/Components/scheduleCard/matchscore.tsx
@@ -200,7 +200,7 @@ export default function OrganiserMatch({
   };
   return (
     <div className={styles.card}>
-      <h2>Match </h2>
+      <h2>Match {matchid}</h2>
       <Image
         src={`/logos/${home_team}.jpeg`}
         alt={`Logo of ${home_team}`}
