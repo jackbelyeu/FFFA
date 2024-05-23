25fb371376ed214aac83b52164f58fb56dc79d84
PRASHANTH REDDY <81088623+prshanthreddy@users.noreply.github.com>
2024-05-23 01:58:55 -0500

    Change to Image componnet and change css

--- src/app/PlayerLogin/page.tsx
+++ src/app/PlayerLogin/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import React from "react";
 
 export default function Teams() {
   const [teams, setTeams] = useState<string[]>([]);
+
   useEffect(() => {
     fetch("/api/teams")
       .then((res) => res.json())
@@ -12,25 +14,30 @@ export default function Teams() {
         setTeams(data.teams);
       });
   }, []);
-
   return (
-    <div style={{ margin: "20px"}}>
-      <h1>Please Select your Team to Login</h1>
-      <div className="container">
-        <div className="row" style={{ padding: "20px" }}>
-          {teams.map((team, index) => (
-            <div className="col-md-4" key={index}>
-              <div className="card border-0">
-                <div className="card-body p-0">
-                  <Link href={`/${team.toLowerCase().replace(/\s+/g, '')}/rsvp`}>
-                    <img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src={`logos/${team}.jpeg`} alt={team} />
-                  </Link>
+    <div className="m-5">
+    <h1 className="text-center text-2xl font-bold mb-5">Please Select your Team to Login</h1>
+    <div className="container mx-auto">
+      <div className="flex flex-wrap -mx-4">
+        {teams.map((team, index) => (
+          <div className="w-full md:w-1/3 px-4 mb-8" key={index}>
+            <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            <Link href={`/${team.toLowerCase().replace(/\s+/g, '')}/rsvp`}>
+                <div className="p-4 flex justify-center items-center">
+                  <Image
+                    src={`/logos/${team}.jpeg`}
+                    alt={team}
+                    width={200}
+                    height={200}
+                    className="rounded-full border-4 border-gray-200 shadow-lg"
+                  />
                 </div>
-              </div>
+              </Link>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
+  </div>
   );
 }
