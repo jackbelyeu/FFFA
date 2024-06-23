ac0ba101963f6f112b351f521f8d732e71de117d
Jack Belyeu <jackbelyeu@gmail.com>
2024-06-23 10:59:27 -0500

    fixes issue 53, changes accordion behavior for user experience

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -37,7 +37,7 @@ const MatchSchedule = () => {
         const pastMatches = matchRows.filter(
           (row: matchRow) => row.date.split("T")[0] < todayDate
         );
-        setPastMatches(pastMatches);
+        setPastMatches(pastMatches.reverse());
 
         const futureMatches = matchRows.filter(
           (row: matchRow) => row.date.split("T")[0] >= todayDate
@@ -59,8 +59,8 @@ const MatchSchedule = () => {
           </h2>
         )}
 
-        <Accordion defaultActiveKey="1">
-          <Accordion.Item eventKey="1">
+        <Accordion defaultActiveKey={["0"]} alwaysOpen>
+          <Accordion.Item eventKey="0">
             <Accordion.Header>
               <h2 className="text-center text-xl font-medium text-blue-500">
                 Upcoming Matches
@@ -84,7 +84,7 @@ const MatchSchedule = () => {
               )}
             </Accordion.Body>
           </Accordion.Item>
-          <Accordion.Item eventKey="2">
+          <Accordion.Item eventKey="1">
             <Accordion.Header>
               <h2 className="text-center text-xl font-medium text-blue-500">
                 Past Matches
